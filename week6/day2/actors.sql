69d5b127f3c20aaba7fcd5143cc491b34744f8da
sharon <sharondoroth@live.com>
2023-08-20 19:37:40 +0300

    Week6day2DailyChallenge

---
+++ week6/day2/actors.sql
@@ -0,0 +1,7 @@
+SELECT * FROM public.actors
+ORDER BY actor_id ASC 
+
+SELECT COUNT(*) FROM actors;
+
+--  Try to add a new actor with some blank fields. What do you think the outcome will be ?
+ERROR
