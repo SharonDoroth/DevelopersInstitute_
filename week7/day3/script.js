f6d88c01a8e8973d93af914ffd60e67649a73026
sharon <sharondoroth@live.com>
2023-08-22 18:52:57 +0300

    Week6day6dailyChallenge

--- week7/day3/script.js
+++ week7/day3/script.js
@@ -1,29 +1,40 @@
-//reverse a single word
-function reverseWord(str) {
-    let returnStr = '';
-    //loop in reverse order
-    for(let i = str.legth-1; i >= 0; i--){
-        returnStr += str[i]
-    }
-    return returnStr
-}
+// //reverse a single word
+// function reverseWord(str) {
+//     let returnStr = '';
+//     //loop in reverse order
+//     for(let i = str.legth-1; i >= 0; i--){
+//         returnStr += str[i]
+//     }
+//     return returnStr
+// }
 
-//console.log(reverseWord('given'));
-//get words from a given string
-function reverseAll(str){
-    let arr = str.split(" ");
-    console.log(arr);
-    let returnArr = [];
-    for(let i = 0; i < arr.length; i++){
-        returnArr.push(reverseWord(arr[i]));
-    }
-    return returnArr.join(' ');
+// //console.log(reverseWord('given'));
+// //get words from a given string
+// function reverseAll(str){
+//     let arr = str.split(" ");
+//     console.log(arr);
+//     let returnArr = [];
+//     for(let i = 0; i < arr.length; i++){
+//         returnArr.push(reverseWord(arr[i]));
+//     }
+//     return returnArr.join(' ');
 
-}
+// }
 
-console.log(reverseAll('reverse each word in a given String'));
-
+// console.log(reverseAll('reverse each word in a given String'));
+//explanation
 
 function x(){
-    
+
 }
+
+const getName = function(name){
+    return name;
+};
+getName('John');
+
+//es6
+//arrow function
+const getXY = (x, y) => x * y;
+
+getXY(3, 4);
