b49f18b5a0a1f7086fe2aa7d2566544003522d2f
sharon <sharondoroth@live.com>
2023-09-06 20:12:32 +0300

    Week9day4ExercisesXP

---
+++ week9/day4/exercises/script1.js
@@ -0,0 +1,19 @@
+const apiUrl =
+  "https://api.giphy.com/v1/gifs/search?q=hilarious&rating=g&api_key=hpvZycW22qCjn5cRM1xtWB8NKq4dQ2My";
+
+const xhr = new XMLHttpRequest();
+
+xhr.open("GET", apiUrl, true);
+
+xhr.onload = function () {
+  if (xhr.status === 200) {
+
+    const response = JSON.parse(xhr.responseText);
+    console.log(response);
+  } else {
+
+    console.error("Failed to fetch data from the API.");
+  }
+};
+
+xhr.send();
