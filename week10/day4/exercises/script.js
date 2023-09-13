402702774c9d62be36c04c33b8876de0697697b0
sharon <sharondoroth@live.com>
2023-09-13 23:54:43 +0300

    week10day04exercises

---
+++ week10/day4/exercises/script.js
@@ -0,0 +1,17 @@
+//Exercise 1: Conversion
+// async function fetchData() {
+//   try {
+//     const response = await fetch("https://www.swapi.tech/api/starships/9/");
+//     const objectStarWars = await response.json();
+//     console.log(objectStarWars.result);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// fetchData();
+
+
+//Exercise 2: Analyze
+//The output will be -calling and resolved
+
